6a96531a48b20efd828f83698e24664e5edfec75
Aldo <aldosteve@hotmail.com>
2026-06-06 16:56:46 -0600

    Eliminando sendri

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://codeandcraft.studio",
   output: "static",
-  adapter: cloudflare({
-    // @ts-ignore
-    platformProxy: { enabled: true },
-  }),
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,1 @@
 /// <reference path="../.astro/types.d.ts" />
-
-interface ImportMetaEnv {
-  readonly SENDGRID_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
-declare module "cloudflare:workers" {
-  export const env: {
-    SENDGRID_API_KEY: string;
-  };
-}
